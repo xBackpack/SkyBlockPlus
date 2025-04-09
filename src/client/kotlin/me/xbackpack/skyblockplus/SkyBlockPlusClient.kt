68d543d5bbe77b9d99e36dcd5a24fa1db0e056cf
xBackpack <jamescheeseman21@gmail.com>
2025-04-09 21:03:18 +0100

    Initial commit

--- src/client/kotlin/me/xbackpack/skyblockplus/SkyBlockPlusClient.kt
+++ src/client/kotlin/me/xbackpack/skyblockplus/SkyBlockPlusClient.kt
@@ -3,7 +3,7 @@ package me.xbackpack.skyblockplus
 import net.fabricmc.api.ClientModInitializer
 
 object SkyBlockPlusClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    override fun onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
 }
