61c31cb51aac3378a94dd680e4ab3201fd646db2
xBackpack <jamescheeseman21@gmail.com>
2025-04-09 20:49:20 +0100

    Initial commit

---
+++ src/client/kotlin/me/xbackpack/skyblockplus/SkyBlockPlusClient.kt
@@ -0,0 +1,9 @@
+package me.xbackpack.skyblockplus
+
+import net.fabricmc.api.ClientModInitializer
+
+object SkyBlockPlusClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/me/xbackpack/skyblockplus/SkyBlockPlusDataGenerator.kt
@@ -0,0 +1,9 @@
+package me.xbackpack.skyblockplus
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object SkyBlockPlusDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
